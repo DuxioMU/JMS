13f574e0f579a14d8f68077f82f8a41856ce3f39
DuxioMU <wwweeencai@163.com>
2017-08-27 16:33:25 +0800

    使用JMS 与Active连接

---
+++ workspace/JMS/src/main/java/com/imooc/jms/queue/AppProducer.java
@@ -0,0 +1,61 @@
+package com.imooc.jms.queue;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+/**
+ * 消息的提供者
+ * @author Administrator
+ *
+ */
+public class AppProducer {
+	
+	
+	@SuppressWarnings("unused")
+	//private static final String url="tcp://192.168.1.100:61616";
+	//连接activemq集群
+	private static final String url=
+			"failover:(tcp://192.168.1.100:61616,"
+			+ "tcp://192.168.1.100:61617,"
+			+ "tcp://192.168.1.100:61618)"
+			+ "?randomize=true";
+	
+	
+	@SuppressWarnings("unused")
+	private static final String queueName="queue-test";
+	
+	public static void main(String[] args) throws JMSException {
+		// 1、创建连接工厂ConnectionFactory
+		ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(url);
+		//2、创建Connection
+		Connection connection = connectionFactory.createConnection();
+		//3、启动连接
+		connection.start();
+		//4 、创建会话
+		Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+		//5、创建一个目标
+		Destination destination = session.createQueue(queueName);
+		//6、创建一个生产者
+		MessageProducer producer = session.createProducer(destination);
+		
+		for(int i= 0;i<100;i++){
+			//7、创建消息
+			TextMessage textMessage = session.createTextMessage("test"+i);
+			//8、发送消息
+			producer.send(textMessage);
+			
+			System.out.println("发送消息"+textMessage.getText());
+		}
+		
+		//9、关闭连接
+		connection.close();
+		
+	}
+}
